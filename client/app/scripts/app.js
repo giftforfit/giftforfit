60c126f6d8a84c3dd99c944334aabfd201755abf
Daniil <daniil@redmart.com>
2015-05-09 14:08:07 +0800

    Frontend skeleton.

---
+++ client/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+angular.module('giftforfitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'pascalprecht.translate',
+    'localization',
+    'restangular'
+  ]);
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name giftforfitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the giftforfitApp
+ */
+angular.module('giftforfitApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
